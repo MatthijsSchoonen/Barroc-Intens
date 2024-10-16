5c31060d00ab35903030a9a7a1085cd17286bc21
Dinan <116496595+DinanII@users.noreply.github.com>
2024-10-16 15:29:48 +0200

    Attempt to add shadow SignIn

--- Barroc-Intens/Barroc-Intens/SignIn.xaml.cs
+++ Barroc-Intens/Barroc-Intens/SignIn.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
@@ -13,7 +14,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
+using Microsoft.UI.Composition;
+using System.Numerics;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -49,5 +51,28 @@ namespace Barroc_Intens
             }
             ErrorMessage.Text = "User not found";
         }
+
+        //private void SignIn_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    AddDropShadow();
+        //}
+
+        //private void AddDropShadow()
+        //{
+        //    var compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+
+        //    var dropShadow = compositor.CreateDropShadow();
+        //    dropShadow.Mask = null;
+        //    dropShadow.Offset = new Vector3(90, 90, 0);
+        //    dropShadow.BlurRadius = 10.0f;
+        //    dropShadow.Color = Microsoft.UI.Colors.Black;
+
+        //    var shadowVisual = compositor.CreateSpriteVisual();
+        //    shadowVisual.Shadow = dropShadow;
+        //    shadowVisual.Size = new Vector2((float)spSignInPanel.ActualWidth, (float)spSignInPanel.ActualHeight);
+
+        //    ElementCompositionPreview.SetElementChildVisual(spSignInPanel, shadowVisual);
+        //}
+
     }
 }
