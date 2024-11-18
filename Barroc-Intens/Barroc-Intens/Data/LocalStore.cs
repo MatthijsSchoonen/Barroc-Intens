ee41edd34d48cbfc6bf3d91f2da51b2b2b36db0f
Dinan <116496595+DinanII@users.noreply.github.com>
2024-11-18 10:42:36 +0100

    Initialized LocalStore Class
    With this class, it is easier to retrieve information (like userdata) throughout the application.

---
+++ Barroc-Intens/Barroc-Intens/Data/LocalStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intens.Data
+{
+    internal class LocalStore
+    {
+        private User LoggedInUser { get; set; }
+
+        internal void SetLoggedInUser(User user)
+        {
+            this.LoggedInUser = user;
+        }
+        internal User GetLoggedInUser()
+        {
+            return this.LoggedInUser;
+        }
+    }
+}
