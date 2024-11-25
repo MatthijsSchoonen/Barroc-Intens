f50f44351c9c6171174742828294b71a93fe7e01
KevDru <126791738+KevDru@users.noreply.github.com>
2024-11-25 22:05:18 +0100

    Updated Invoice model to also save VAT and Connection costs

--- Barroc-Intens/Barroc-Intens/Data/Invoice.cs
+++ Barroc-Intens/Barroc-Intens/Data/Invoice.cs
@@ -9,9 +9,13 @@ namespace Barroc_Intens.Data
     public class Invoice
     {
         public int Id { get; set; }
-        public DateOnly Date {  get; set; }
+        public DateOnly Date { get; set; }
         public DateOnly? PaidAt { get; set; }
         public int ContractId { get; set; }
         public Contract Contract { get; set; }
+
+        public int VAT { get; set; }
+
+        public decimal ConnectionCost { get; set; } 
     }
 }
