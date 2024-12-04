a33c5c10ecb210354b243213113924a15fce84b4
Dinan <116496595+DinanII@users.noreply.github.com>
2024-12-04 12:54:05 +0100

    Base Createform Complete

--- Barroc-Intens/Barroc-Intens/Data/PurchaseOrder.cs
+++ Barroc-Intens/Barroc-Intens/Data/PurchaseOrder.cs
@@ -9,7 +9,7 @@ namespace Barroc_Intens.Data
     internal class PurchaseOrder
     {
         public int Id { get; set; }
-        public ICollection<Product> Products { get; set; }
+        public ICollection<Product> Products { get; set; } = [];
         public DateTime OrderedAt { get; set; }
         public int TotalPrice { get; set; }
         public int StatusId { get; set; }
--- Barroc-Intens/Barroc-Intens/Views/PurchaseViews/PurchasesView.xaml.cs
+++ Barroc-Intens/Barroc-Intens/Views/PurchaseViews/PurchasesView.xaml.cs
@@ -1,3 +1,4 @@
+using Barroc_Intens.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -7,6 +8,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,14 +25,56 @@ namespace Barroc_Intens.Views.PurchaseViews
     /// </summary>
     public sealed partial class PurchasesView : Page
     {
+        ObservableCollection<Product> Products = new ObservableCollection<Product>();
+        PurchaseOrder PurchaseOrder = new();
+        Product SelectedProduct = new();
         public PurchasesView()
         {
             this.InitializeComponent();
+            List<Product> productList = new();
+            using (AppDbContext appDbContext = new())
+            {
+                productList = appDbContext.Products.ToList();
+            }
+
+            foreach (Product product in productList)
+            {
+                Products.Add(product);
+            }
+
+            FProductSelector.ItemsSource = Products;
         }
 
+        private void MakePurchaseToggle_Click(object sender, RoutedEventArgs e)
+        {
+            if (CreatePurchase.Visibility == Visibility.Visible)
+            {
+                CreatePurchase.Visibility = Visibility.Collapsed;
+                return;
+            }
+            CreatePurchase.Visibility = Visibility.Visible;
+        }
+        
         private void OnProductSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Product selectedProduct = FProductSelector.SelectedItem as Product;
 
+            if (selectedProduct != null) { 
+                ProductTitle.Text = selectedProduct.Name.ToString();
+                ProductDescription.Text = selectedProduct.Description.ToString();
+                //PurchaseOrder.Products.Add(selectedProduct);
+                SelectedProduct = selectedProduct;
+                InfoPanel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void OnAddProductClick(object sender, RoutedEventArgs e) { 
+            string fProdAmount = FProdAmount.Text;
+            if (int.TryParse(fProdAmount, out int prodAmount))
+            {
+                SelectedProduct.Stock = prodAmount;
+            }
+            PurchaseOrder.Products.Add(SelectedProduct);
         }
     }
 }
