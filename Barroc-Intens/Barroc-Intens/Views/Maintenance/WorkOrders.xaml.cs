6897922aafecac4fdfd51a6df92375c5a7971638
Dinan <116496595+DinanII@users.noreply.github.com>
2024-12-12 12:05:17 +0100

    Created WorkOrder Page & Link from Maintenance Dashboard

--- Barroc-Intens/Barroc-Intens/Views/Maintenance/WorkOrders.xaml.cs
+++ Barroc-Intens/Barroc-Intens/Views/Maintenance/WorkOrders.xaml.cs
@@ -1,4 +1,3 @@
-using Barroc_Intens.Views.Maintenance;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -17,26 +16,16 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Barroc_Intens.Dashboards
+namespace Barroc_Intens.Views.Maintenance
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Maintenance : Page
+    public sealed partial class WorkOrders : Page
     {
-        public Maintenance()
+        public WorkOrders()
         {
             this.InitializeComponent();
-        }
-
-        private void BtnVisitOverviewNav_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(VisitOverview));
-        }
-
-        private void BtnVisitWorkOrderNav_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(WorkOrder));
         }
     }
 }
