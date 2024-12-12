37dfe1d6000065d0785ed74911d67e9608b40281
Dinan <116496595+DinanII@users.noreply.github.com>
2024-12-12 12:19:11 +0100

    Initialized Classes

---
+++ Barroc-Intens/Barroc-Intens/Data/WorkOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intens.Data
+{
+    internal class WorkOrder
+    {
+        public int Id { get; set; }
+        public int MaintenanceAppointmentId { get; set; }
+        public MaintenanceAppointment MaintenanceAppointment { get; set; }
+    }
+}
---
+++ Barroc-Intens/Barroc-Intens/Data/WorkOrderMat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intens.Data
+{
+    internal class WorkOrderMat
+    {
+        public int Id { get; set; }
+        public int WorkOrderId { get; set; }
+        public WorkOrder WorkOrder { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
