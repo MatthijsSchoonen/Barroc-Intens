13efe897847f8f821659ab61678d44dd5484bab4
matthijs <113593159+MatthijsSchoonen@users.noreply.github.com>
2024-12-17 22:45:28 +0100

    factuur gegevens zien

--- Barroc-Intens/Barroc-Intens/Views/Customer/Customer.xaml.cs
+++ Barroc-Intens/Barroc-Intens/Views/Customer/Customer.xaml.cs
@@ -1,63 +1,61 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Barroc_Intens.Data;
-using System.Diagnostics.Contracts;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System.Linq;
 
 namespace Barroc_Intens.Dashboards
 {
     public partial class Customer : Page
     {
+        private AppDbContext _context;
+
         public Customer()
         {
             InitializeComponent();
+            _context = new AppDbContext();
             LoadContracts();
         }
 
-        // Context voor Pomelo
-        private AppDbContext _context;
-
         public void LoadContracts()
         {
-            using (var context = new AppDbContext())
-            {
-                var leaseContracts = context.Contracts.Include(w => w.Company).ToList();
+            // Load all contracts and include company information
+            var leaseContracts = _context.Contracts
+                                         .Include(c => c.Company)
+                                         .ToList();
 
-                ContractListBox.ItemsSource = leaseContracts;
+            ContractListBox.ItemsSource = leaseContracts;
+        }
+
+        private void OnContractSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (ContractListBox.SelectedItem is Contract selectedContract)
+            {
+                // Query invoices related to the selected contract
+                var invoices = _context.Invoices
+                                       .Include(i => i.Company)
+                                       .Include(i => i.Contract)
+                                       .Where(i => i.ContractId == selectedContract.Id)
+                                       .ToList();
+
+                // Bind invoices to the InvoiceListView
+                InvoiceListView.ItemsSource = invoices;
             }
         }
 
-
-        private void OnContractSelected(object sender, RoutedEventArgs e)
+        private void OnInvoiceSelected(object sender, SelectionChangedEventArgs e)
         {
-            if (ContractListBox.SelectedItem != null)
+            if (InvoiceListView.SelectedItem is Invoice selectedInvoice)
             {
-                int contractId = (int)ContractListBox.SelectedItem;
+                // Load the products associated with the selected invoice
+                var invoiceProducts = _context.InvoiceProducts
+                                              .Include(ip => ip.Product)
+                                              .Where(ip => ip.InvoiceId == selectedInvoice.Id)
+                                              .ToList();
 
-                
-                var contract = _context.Contracts
-                    .Include(c => c.Company)
-                    .Include(c => c.Products)
-                    .FirstOrDefault(c => c.Id == contractId);
-
-            
+                // Bind to the InvoiceProductsListView
+                InvoiceProductsListView.ItemsSource = invoiceProducts;
             }
         }
     }
 }
-
